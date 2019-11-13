68ac29ec71e12765f0211188d2f2760142798c97
NechamaVeber <Nechama.Shraiber@seldatinc.com>
2019-11-13 11:51:56 +0200

    dates

--- angular/src/app/login/login.service.ts
+++ angular/src/app/login/login.service.ts
@@ -12,16 +12,16 @@ baseRoute:string="http://localhost:58516/api/";
    //loginSample(user:User){
     //return  this.http.post(this.baseRoute+"User");
   //}
-  login(user:User): any {      
-    const body = new HttpParams()          
-    .set('grant_type', 'password')          
-    .set('username', user.email)    
-    .set('password', user.password)    
-    return this.http.post('http://localhost:58516/token', body.toString(), {observe: 'response',    
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },    
-    });  
-  }  
-  // login(user:User){
-  //    return  this.http.post(this.baseRoute+"User?subItem=login",user);
-  //  }
+  // login(user:User): any {      
+  //   const body = new HttpParams()          
+  //   .set('grant_type', 'password')          
+  //   .set('username', user.email)    
+  //   .set('password', user.password)    
+  //   return this.http.post('http://localhost:58516/token', body.toString(), {observe: 'response',    
+  //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },    
+  //   });  
+  // }  
+   login(user:User){
+      return  this.http.post(this.baseRoute+"User?subItem=login",user);
+    }
 }
